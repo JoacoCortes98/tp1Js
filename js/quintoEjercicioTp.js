0819f3038db7a28c0f137821bc6a2d85c9da5667
Joaquin Cortes Carbajo <joaquincortescarbajo@gmail.com>
2022-07-04 17:04:44 -0300

    termine los 5 primeros ejercicios

--- js/quintoEjercicioTp.js
+++ js/quintoEjercicioTp.js
@@ -1,3 +1,3 @@
-let numeros = "1"
-numeros = parseInt(prompt("ingrese un numero")) + parseInt(prompt("ingrese un numero"));
-document.write("<p>Resultado " + (numeros) + "</p>");
+let numero1 = parseInt(prompt('Ingrese un primer numero'));
+let numero2 = parseInt(prompt('Ingrese un segundo numero'));
+document.write('El resultado es ' + (numero1 + numero2));
